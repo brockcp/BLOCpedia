a2b3237e676aa7973a8e20f072dae82ca6125023
brockcp <brockpatterson@gmail.com>
2017-11-14 16:45:02 -0800

    8_private_wikis

---
+++ NOTES.rb
@@ -0,0 +1,6 @@
+APPLICATIONCONTROLLER
+#when :show removed -> click on wiki and must login.
+#when :index removed -> home disappears
+
+if wiki is private is current user and current user is wiki user! 
+OR the wiki is private and  current user!
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,24 +2,31 @@
 require 'random_data'
 require 'faker'
 
-Faker::UniqueGenerator.clear
 
-5.times do
-    User.create!(
-        email: Faker::Internet.unique.email,
-        password: Faker::Internet.password(6, 14)
-    )
-end
-users= User.all
+  User.create!(
+    email: 'admin@example.com',
+    password: 'helloo',
+    role: 'admin'
+  )
 
-5.times do
-    Wiki.create!(
-        user: users.sample,
-        title: Faker::GameOfThrones.character,
-        body: Faker::GameOfThrones.quote
-    )
-end
+# Faker::UniqueGenerator.clear
+#
+# 5.times do
+#     User.create!(
+#         email: Faker::Internet.unique.email,
+#         password: Faker::Internet.password(6, 14)
+#     )
+# end
+# users= User.all
+#
+# 5.times do
+#     Wiki.create!(
+#         user: users.sample,
+#         title: Faker::GameOfThrones.character,
+#         body: Faker::GameOfThrones.quote
+#     )
+# end
 
 puts "Seed finished"
 puts "#{User.count} users created"
-puts "#{Wiki.count} wikis created"
+#Zputs "#{Wiki.count} wikis created"
